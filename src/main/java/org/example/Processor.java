2fadc59fc6e4ce543c0052e61095db092783b103
Nazira Aisina <aisinazira04@gmail.com>
2022-10-19 20:49:04 +0600

    Initial commit

---
+++ src/main/java/org/example/Processor.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * Processor of HTTP request.
+ */
+public class Processor {
+    private final Socket socket;
+    private final HttpRequest request;
+
+    public Processor(Socket socket, HttpRequest request) {
+        this.socket = socket;
+        this.request = request;
+    }
+
+    public void process() throws IOException {
+        // Print request that we received.
+        System.out.println("Got request:");
+        System.out.println(request.toString());
+        System.out.flush();
+
+        // To send response back to the client.
+        PrintWriter output = new PrintWriter(socket.getOutputStream());
+
+        // We are returning a simple web page now.
+        output.println("HTTP/1.1 200 OK");
+        output.println("Content-Type: text/html; charset=utf-8");
+        output.println();
+        output.println("<html>");
+        output.println("<head><title>Hello</title></head>");
+        output.println("<body><p>Hello, world!</p>");
+        if (request.getRequestLine().toString().equals("GET /create/itemid HTTP/1.1")){
+            output.println("<p>Created</p>");
+        }else if(request.getRequestLine().toString().equals("GET /delete/itemid HTTP/1.1")){
+            output.println("<p>Deleted</p>");
+        }else if(request.getRequestLine().toString().equals("GET //exec/params HTTP/1.1")){
+            output.println("<p>Executed</p>");
+        }
+        output.println("</body></html>");
+        output.flush();
+
+        socket.close();
+    }
+    public void findallprime(){
+
+    }
+}
